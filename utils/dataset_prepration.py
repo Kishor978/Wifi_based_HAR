040d81cc85a9e91346df052b1007c18e9804d446
Kishor978 <thaunnakishor@gmail.com>
2025-02-08 21:56:30 +0545

    removing the the get_up and get_down class form dataset

--- utils/dataset_prepration.py
+++ utils/dataset_prepration.py
@@ -37,35 +37,45 @@ def read_csi_data_from_csv(path_to_csv, is_five_hhz=False, antenna_pairs=4):
     return amplitudes, phases
 
 
-def read_labels_from_csv(path_to_csv):
+def read_labels_from_csv(path_to_csv,expected_length):
     """
-    Read labels(human activities) from csv file
-
+    Read labels (human activities) from csv file and remove unwanted classes.
+    
     :param path_to_csv: string
-    :return: labels, np.array of shape(data_len, 1)
+    :return: filtered labels, np.array of shape (data_len, 1)
     """
-
     data = pd.read_csv(path_to_csv, header=None).values
     labels = data[:, 1]
+    
+    # Ensure labels match the expected length
+    if len(labels) > expected_length:
+        labels = labels[:expected_length]  # Trim extra labels if any
+    elif len(labels) < expected_length:
+        raise ValueError(f"Label file {path_to_csv} has fewer rows than CSI data!")
 
-    return labels
+
+    # Filter out 'get_down' and 'get_up'
+    valid_indices = ~np.isin(labels, ["get_down", "get_up"])
+    
+    return labels[valid_indices], valid_indices  # Also return indices to filter amplitudes & phases
+
 
 def read_all_data_from_files(paths, is_five_hhz=True, antenna_pairs=4):
     """
-    Read csi and labels data from all folders in the dataset
+    Read CSI and labels data from all folders in the dataset while removing 'get_down' and 'get_up'.
 
     :return: amplitudes, phases, labels all of shape (data len, num of subcarriers)
     """
-
     final_amplitudes, final_phases, final_labels = np.empty((0, antenna_pairs * SUBCARRIES_NUM_FIVE_HHZ)), \
                                                    np.empty((0, antenna_pairs * SUBCARRIES_NUM_FIVE_HHZ)), \
                                                    np.empty((0))
 
-    for index, path in enumerate(paths):
+    for path in paths:
         amplitudes, phases = read_csi_data_from_csv(os.path.join(path, "data.csv"), is_five_hhz, antenna_pairs)
-        labels = read_labels_from_csv(os.path.join(path, "label.csv"))
+        labels, valid_indices = read_labels_from_csv(os.path.join(path, "label.csv"),len(amplitudes))
 
-        amplitudes, phases = amplitudes[:-1], phases[:-1]  # fix the bug with the last element
+        # Apply the filter
+        amplitudes, phases = amplitudes[valid_indices], phases[valid_indices]
 
         final_amplitudes = np.concatenate((final_amplitudes, amplitudes))
         final_phases = np.concatenate((final_phases, phases))
@@ -82,4 +92,3 @@ def read_all_data(is_five_hhz=True, antenna_pairs=4):
             all_paths.append(os.path.join(DATASET_FOLDER, room, subroom))
 
     return read_all_data_from_files(all_paths, is_five_hhz, antenna_pairs)
-
